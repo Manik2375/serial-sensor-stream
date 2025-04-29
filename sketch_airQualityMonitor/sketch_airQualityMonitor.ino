e9a43e80628478ffab4f326e3f3a13e15d16d04b
Manik2375 <manikkpr2375@gmail.com>
2025-04-29 19:10:26 +0530

    add sketch for oled display

--- sketch_airQualityMonitor/sketch_airQualityMonitor.ino
+++ sketch_airQualityMonitor/sketch_airQualityMonitor.ino
@@ -1,35 +1,109 @@
 #include <DHT.h>
 
-#define DHTPIN 2        
-#define DHTTYPE DHT11   
-#define MQ135_PIN A0    // Analog pin where MQ135 is connected
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+#define MQ135_PIN A0
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 DHT dht(DHTPIN, DHTTYPE);
 
+float temp;
+float humidity;
+int airQualityRaw;
+
 void setup() {
-  Serial.begin(9600);      
+  Serial.begin(9600);
+
+  pinMode(DHTPIN, INPUT);
   dht.begin();
-  delay(1000);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;
+  }
+
+  display.clearDisplay();
+
+  display.display();
+  display.setTextColor(SSD1306_WHITE);
+  display.setTextSize(2);
+  display.setCursor(50, 0);
+  display.println("Air");
+  display.setTextSize(1);
+  display.setCursor(23, 20);
+  display.println("Quality monitor");
+  display.display();
+
+  delay(1200);
+
+  display.clearDisplay();
+
   pinMode(LED_BUILTIN, OUTPUT);
-  Serial.println("Temperature, Humidity, and Air Quality Monitor");
+  Serial.println(F("Temperature, Humidity, and Air Quality Monitor"));
 }
 
-void loop() {
-  float temp = dht.readTemperature();    
-  float humidity = dht.readHumidity();
-  int airQualityRaw = analogRead(MQ135_PIN); // Read raw analog value from MQ135
+void dhtSensorRead() {
+  temp = dht.readTemperature();
+  humidity = dht.readHumidity();
 
   if (isnan(temp) || isnan(humidity)) {
     Serial.println("Failed to read from DHT11 sensor!");
-    delay(1000);
     return;
   }
 
-  Serial.print("Temperature: ");
+
+  display.setTextColor(SSD1306_WHITE);
+  display.setTextSize(1);
+
+  display.setCursor(0, 26);
+  display.print("Temp: ");
+  display.print(temp);
+  display.println(" C");
+
+  display.setCursor(0, 36);
+  display.print("Humidity: ");
+  display.print(humidity);
+  display.print(" %");
+}
+
+
+
+void airSensorRead() {
+  airQualityRaw = analogRead(MQ135_PIN);
+
+  display.setTextColor(SSD1306_WHITE);
+  display.setTextSize(1);
+  display.setCursor(0, 5);
+  display.print("Air Quality: ");
+  display.println(airQualityRaw);
+}
+
+
+void loop() {
+
+  display.clearDisplay();
+  dhtSensorRead();
+  airSensorRead();
+  display.display();
+
+
+  Serial.print(F("Temperature: "));
   Serial.print(temp);
-  Serial.print("°C\tHumidity: ");
+  Serial.print(F("°C\tHumidity: "));
   Serial.print(humidity);
-  Serial.print("%\tAir Quality (Raw): ");
+  Serial.print(F("%\tAir Quality (Raw): "));
   Serial.println(airQualityRaw);
 
   const char state = Serial.read();
@@ -40,5 +114,5 @@ void loop() {
     digitalWrite(LED_BUILTIN, LOW);
   }
 
-  delay(1000); 
+  delay(500);
 }
